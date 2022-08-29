e2ecde12daf3da66aea8893d1b10cace1006a276
andrew-pahomov <andrew-pahomov@yandex.ru>
2022-08-29 09:38:17 +0300

    обновил проект

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -4,22 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 class PostmanEchoTest {
+
     @Test
     void shouldReturnSendData() {
-        // Given - When - Then
-        // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("Data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-                // Выполняемые действия
+                .body("Data")
                 .when()
                 .post("/post")
-                // Проверки
                 .then()
                 .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("postmanecho.schema.json"))
-        ;
+                .body("data", equalTo("Data"))
+                .body(matchesJsonSchemaInClasspath("postmanecho.schema.json"));
     }
 }
